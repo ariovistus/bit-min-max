b295654fc6411cf178c9a4f92e4ffedcdda61f86
Ellery Newcomer <ellery-newcomer@utulsa.edu>
2011-02-11 17:04:46 -0600

    trying to clarify the document

--- bitall.d
+++ bitall.d
@@ -1,7 +1,46 @@
-import std.traits;
-import std.stdio;
-import std.algorithm;
-import std.conv;
+/++
+ + author:         Ellery Newcomer
+ + license:        BSD 
+ + copyright:      Copyright (c) 2011 Ellery Newcomer All rights reserved.
+ +/
+
+/+ bsd license
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+1. Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+3. The name of the author may not be used to endorse or promote products
+derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ +/
+module bitall;
+/++ 
+ + This module contains algorithms for finding the minimum and maximum of
+ + an expression a op b, where
+ + a and b range through the integer intervals [amin, amax], [bmin, bmax].
+ + op is one of the binary operators:
+ +    & | ^ 
+ +    for either signed or unsigned arithmetic
+ +
+ + There should be an accompanying pdf file which describes the derivation
+ + of the following algorithms. Refer to it.
+ +/
+
+import std.algorithm, std.conv, std.stdio, std.traits;
 static import std.intrinsic;
 
 template signed(T){
@@ -254,8 +293,8 @@ in{
         if( b0 ) bmin = 0;
         return cast(I) maxor!(unsigned!I)(amin, amax, bmin, bmax);
     }else{
-        I x0 = (~amin&amax&~bmax)|(~bmin&bmax&~amax);
         I x1 = (~amin&amax& bmax)|(~bmin&bmax& amax);
+        I x0 = (~amin&amax&~bmax)|(~bmin&bmax&~amax);
         x1 |= hbmask(x0)&amax&bmax;
         return amax|bmax|hbmask(x1);
     }
